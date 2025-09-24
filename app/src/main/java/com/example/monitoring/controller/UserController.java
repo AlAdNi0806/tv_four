52893bb3fc08d06b716784870ed378a247f7470e
AlAdNi0806 <alschr0806@gmail.com>
2025-09-24 09:39:56 +0300

    test

--- app/src/main/java/com/example/monitoring/controller/UserController.java
+++ app/src/main/java/com/example/monitoring/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.monitoring.controller;
 
 import com.example.monitoring.entity.User;
 import com.example.monitoring.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +12,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
     
     @GetMapping
     public List<User> getAllUsers() {
-        return userService.getAllUsers();
+        logger.info("Getting all users - testing Graylog integration");
+        List<User> users = userService.getAllUsers();
+        logger.info("Retrieved {} users", users.size());
+        return users;
     }
     
     @GetMapping("/{id}")
